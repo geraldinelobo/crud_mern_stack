a7e2b53935b776bb6e07e25df8382c4178560f09
geraldinelobo <geraldinelobo21@gmail.com>
2026-03-01 00:05:01 -0300

    Creando componentes y agregando rutas

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import ListaUsuarios from './ListaUsuarios';
+import AgregarUsuario from './AgregarUsuario';
+import EditarUsuario from './EditarUruario';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <h1>CRUD MERN Stack</h1>
+
+
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<ListaUsuarios/>} exact></Route>
+        <Route path='/agregarusuario' element={<AgregarUsuario/>} exact></Route>
+        <Route path='/editarusuario' element={<EditarUsuario/>} exact></Route>
+      </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
